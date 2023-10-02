ee9c4d1c67cb8d9184d5430640da134945182864
Bruna <79263157+BrunaAlvesL@users.noreply.github.com>
2023-10-02 14:23:18 -0300

    2/10 - 1

--- EXERCICIO-02/index2.js
+++ EXERCICIO-02/index2.js
@@ -1,5 +1,7 @@
 /* 
-Escreva um programa que permita inserir o nome e o poder de ataque de um personagem, depois o nome, a quantidade de pontos de vida, o poder de defesa de outro personagem e se ele possui um escudo, e então calcule a quantidade de dano causado baseado nas seguintes regras:
+Escreva um programa que permita inserir o nome e o poder de ataque de um personagem, depois o nome, 
+a quantidade de pontos de vida, o poder de defesa de outro personagem e se ele possui um escudo, 
+e então calcule a quantidade de dano causado baseado nas seguintes regras:
 
 - Se o poder de ataque for maior do que a defesa e o defensor não possuir um escudo, o dano causado será igual a diferença entre o ataque e a defesa.
 - Se o poder de ataque for maior do que a defesa e o defensor possuir um escudo, o dano causado será igual a metade da diferença entre o ataque e a defesa.
