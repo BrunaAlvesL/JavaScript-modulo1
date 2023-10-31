07b16428c3da0b0ddca6740c2a8ac7842b744a6f
Bruna <79263157+BrunaAlvesL@users.noreply.github.com>
2023-10-31 10:22:09 -0300

    f

---
+++ teste/index.js
@@ -0,0 +1,12 @@
+const unidade = prompt('Digite em valor em metros:')
+
+const mm = unidade
+const cm = unidade
+const dm = unidade
+const dam = unidade
+const hm = unidade
+const km = unidade
+
+switch (unidade) {
+    
+}
