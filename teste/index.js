ba075e68c440610fccd06a41d45adee134038aed
Bruna <79263157+BrunaAlvesL@users.noreply.github.com>
2023-11-24 17:37:22 -0300

    commit

--- teste/index.js
+++ teste/index.js
@@ -1,12 +1,35 @@
-const unidade = prompt('Digite em valor em metros:')
+let fila = []
+let opcao = ""
 
-const mm = unidade
-const cm = unidade
-const dm = unidade
-const dam = unidade
-const hm = unidade
-const km = unidade
+do{
+    let pacientes = ""
+    for(let i = 0; i < fila.length; i++) {
+        pacientes +=(i + 1) + "° - " + fila[i] + "\n"
+    }
 
-switch (unidade) {
+    opcao = prompt("pacientes:\n" + pacientes +
+    "\nEscolha uma ação: \n1 - Novo paciente\n2 - Consultar paciente\n3 - Sair")
+
+
+    switch (opcao) {
+        case "1":
+            const novoPaciente = prompt("Qual é o nome do paciente")
+            fila.push(novoPaciente)
+            break
+        case "2":
+            const pacienteConsultado = fila.shift()
+            if (!pacienteConsultado) {
+                alert("Não há mais pacientes na fila")
+            }else {
+                alert(pacienteConsultado + "foi removido da fila.")
+            }
+            break
+        case "3":
+            alert("Encerrado")
+            break
+        default:
+            alert("Opção invalida")
+            break
+    }
     
-}
+} while(opcao !== "3");
